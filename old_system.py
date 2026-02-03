eeea21847489a96b09a5a8433176f72aa4be2bd3
pgrewal33 <pg10@kent.ac.uk>
2026-02-03 20:54:59 +0000

    fixed the count

--- old_system.py
+++ old_system.py
@@ -59,7 +59,7 @@ def run_system_monolith():
             for rank in r:
                 if rank == "Captain" or "Commander": 
                     count = count + 1
-            print("High ranking officers: " + count) 
+            print(f"High ranking officers:  + {count}") 
             
         elif opt == "5":
             print("Shutting down.")
