3d2f0fbb13f284360bc616cfc1f09273bbcab29b
pgrewal33 <pg10@kent.ac.uk>
2026-02-04 13:33:03 +0000

    added database

---
+++ fleet_manager.py
@@ -0,0 +1,8 @@
+def init_database():
+    names = ["Picard", "Riker", "Data", "Worf", "Forge"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
+    divisions = ["Command", "Command", "Operations", "Security", "operations"]
+    ids = ["001", "002", "003", "004", "005"]
+
+    return names, ranks, divisions, ids
+
